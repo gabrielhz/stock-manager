ef60b9db1d5bcd23b1cacd3d011884c4f526243b
Gabriel Ramos <gabrielramoscardoso@hotmail.com>
2023-07-11 18:45:43 -0300

    now using mysql

--- website/__init__.py
+++ website/__init__.py
@@ -4,13 +4,13 @@ from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
+DB_NAME = "database"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'PWAUJSDHJ213'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://root:salve@localhost/{DB_NAME}'
     db.init_app(app)
 
     from .views import views
@@ -36,6 +36,5 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         print("Database created successfully")
